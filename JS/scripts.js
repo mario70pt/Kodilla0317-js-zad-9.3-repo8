7b898e5ff42ef09b0df51b5b414c6bfab9b5af4c
mario70pt <mario70pt@users.noreply.github.com>
2017-04-29 21:22:59 +0200

    first commit

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.'
+console.log(text)
+var animal = 'Zielone słonie'
+console.log(animal)
+var bigAnimal = animal.toUpperCase('Zielone słonie')
+console.log(bigAnimal)
+var textChange = text.replace('Papugi', 'ZIELONE SŁONIE')
+console.log(textChange)
+console.log(textChange.length/2)
+var textOfPart = textChange.substr(0, 45)
+console.log(textOfPart)
